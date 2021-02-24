16d9c5cfe45b3b76142f771f88de8cbbde97919f
mrrioja <lulyrioja@gmail.com>
2021-02-24 00:55:52 -0300

    :alien: Creating user table and routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
 import "./database";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Salve quebrada" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
