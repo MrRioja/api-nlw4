43e2aef2c3c77175968a3ffe3ccf9d1c211961e5
mrrioja <lulyrioja@gmail.com>
2021-02-28 00:05:59 -0300

    :alien: Inserting the NPS calculation

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -28,22 +28,24 @@ class SendMailController {
       return res.status(400).json({ error: "Survey does not exists" });
     }
 
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: { user_id: user.id, value: null },
+      relations: ["user", "survey"],
+    });
+
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
-      user_id: user.id,
+      id: "",
       link: process.env.URL_MAIL,
     };
 
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{ user_id: user.id }, { value: null }],
-      relations: ["user", "survey"],
-    });
-
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
+
       await SendMailService.execute(email, survey.title, variables, npsPath);
 
       return res.json(surveyUserAlreadyExists);
@@ -55,6 +57,7 @@ class SendMailController {
     });
 
     await surveysUsersRepository.save(surveyUser);
+    variables.id = surveyUser.id;
 
     await SendMailService.execute(email, survey.title, variables, npsPath);
 
--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -30,7 +30,7 @@ class SurveyUser {
   survey: Survey;
 
   @Column()
-  value: string;
+  value: number;
 
   @CreateDateColumn()
   created_at: Date;
